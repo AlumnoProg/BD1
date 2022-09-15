7ba1781fdc62191c02557fbd363ba6e685a63b5d
AlumnoProg <113617007+AlumnoProg@users.noreply.github.com>
2022-09-15 17:27:30 -0300

    Add files via upload

---
+++ Ej1.pas
@@ -0,0 +1,417 @@
+program Ej1;
+uses crt, dos, sysutils;
+
+type
+	regCliente = record
+		//Posicion: Integer;
+		Apellido: String[50];
+		Nombre: String[50];
+		Codigo: String[4];
+		Activo: Boolean;
+	end;
+
+type
+	archivoClientes = file of regCliente;
+
+//--------------------------------------------------------------------
+//VALIDAR OPCION
+
+function ValorEntero(texto: string): integer;
+var
+  valor, codigoDeError: integer;
+begin
+  valor:= 0;
+  val(texto, valor, codigoDeError);
+  ValorEntero := valor;
+end;
+
+Function ValidarOpcion(Op: String): Boolean;
+var
+	i, codigoDeError: integer;
+begin
+  
+	ValidarOpcion:= False;
+	i:= 0;
+	val(Op, i, codigoDeError);
+	if (codigoDeError = 0) then begin
+		if ((i >= 0) and (i <= 4)) then
+	  		ValidarOpcion:= True;
+	end;		
+
+end;
+
+
+//--------------------------------------------------------------------
+//ValidarCodigo
+
+Function VCod(Cod: String): Boolean;
+var
+	i, codigoDeError: integer;
+begin
+
+	VCod:= False;
+	i:= 0;
+	val(Cod, i, codigoDeError);
+	if (codigoDeError = 0) then begin
+	  if (Length(Cod) <= 4) then
+		  if ((i >= 0) and (i <= 9999)) then
+			VCod:= True;
+	end;
+
+end;
+
+Procedure ValidarCodigo(var Codigo: String);
+begin
+  
+	while (VCod(Codigo) = False) do begin
+	  WriteLn('Error, ingrese un numero de hasta 4 digitos');
+	  readln(Codigo);
+	end;
+
+end;
+
+
+//--------------------------------------------------------------------
+//VALIDAR QUE SOLO SE INGRESEN LETRAS
+
+function revisarContenido(var Palabra:String) : Boolean;
+var
+  i:Integer;
+  Flag: Boolean;
+begin
+  Flag:= True;
+  if ((Palabra <> '') and (Palabra <> ' ')) then begin
+ 	for i:=1 to Length(Palabra) do
+    	if not (Palabra[i] in ['A'..'Z', 'a'..'z']) then 
+  		Flag:= False;
+	end else 
+		Flag:= False;
+  revisarContenido:=Flag;
+end;
+
+Procedure ValidarPalabra(Var Palabra: String);
+begin
+  while (revisarContenido(Palabra) = False) do begin
+    writeln('Error, ingrese solo letra');
+    readln(Palabra);
+  end;
+
+end;
+
+
+//--------------------------------------------------------------------
+//ValidarQueSeaUnoDos
+Procedure ValidarUnoDos(var dato: String);
+begin
+  
+	while ((dato <> '1') and (dato <> '2')) do begin
+	  writeln('Error, ingrese 1 o 2');
+	  readln(dato);
+	end;
+
+end;
+
+
+//--------------------------------------------------------------------
+//MOSTRAR DATOS DEL ARCHIVO
+
+Procedure MostrarDatos(var Client: archivoClientes);
+var	
+	Cliente: regCliente;
+	Posicion: Integer;
+begin
+
+	Posicion:= 1;
+	reset(Client);
+	WriteLn('--------------------------------------');
+	writeln('Datos del archivo');
+	WriteLn('--------------------------------------');
+	writeln(' ');
+	while (not EOF(Client)) do begin
+	  	read(Client, Cliente);
+		if (Cliente.Activo <> False) then begin
+	  		WriteLn('Posicion:   ', Posicion);
+	 		WriteLn('Apellido:   ', Cliente.Apellido);
+	  		WriteLn('Nombre:     ', Cliente.Nombre);
+	  		WriteLn('Cliente:     ', Cliente.Codigo);
+			WriteLn('  ');
+			WriteLn('--------------------------------------');
+			WriteLn('  ');
+			Posicion:= Posicion + 1;
+		end;
+	end;
+	close(Client);
+
+end;
+
+
+//--------------------------------------------------------------------
+//BUSCAR SI ESTA EN EL ARCHIVO
+
+Function Esta(Var Client: archivoClientes; Cliente: regCliente): Boolean;
+var
+	Flag: Boolean;
+	ClienteAux: regCliente;
+begin
+
+	Flag:= False;
+	reset(Client);
+	while ((not EOF(Client)) and (Flag = False)) do begin
+		Read(Client, ClienteAux);
+		if ((Cliente.Apellido = ClienteAux.Apellido) and (Cliente.Nombre = ClienteAux.Nombre) and (Cliente.Codigo = ClienteAux.Codigo) and (ClienteAux.Activo = True)) then
+			Flag:= True;
+	end;
+	Close(Client);
+	Esta:= Flag;
+
+end;
+
+
+//--------------------------------------------------------------------
+//ALTA
+
+Procedure Alta(var Client: archivoClientes);
+var
+	Ape, Nom, Cod: String;
+	Cliente: regCliente;
+begin
+  
+	writeln('Alta: ');
+	writeln('Ingrese apellido del cliente');
+	readln(Ape);
+	ValidarPalabra(Ape);
+
+	writeln('Ingrese nombre del cliente');
+	readln(Nom);
+	ValidarPalabra(Nom);
+
+	writeln('Ingrese codigo del cliente');
+	readln(Cod);
+	ValidarCodigo(Cod);
+
+	Cliente.Apellido:= Ape;
+	Cliente.Nombre:= Nom;
+	Cliente.Codigo:= Cod;
+	Cliente.Activo:= True;
+	reset(Client);
+	Seek(Client, FileSize(Client));
+	write(Client, Cliente);
+	WriteLn('Cliente dado de alta con exito');
+	Close(Client);
+
+end;
+
+
+//--------------------------------------------------------------------
+//BAJA
+
+Procedure Baja(var Client: archivoClientes);
+var	
+	Cliente, ClienteAux: regCliente;
+	Nom, Ape, Cod: String;
+	Flag: Boolean;
+begin
+
+	Flag:= False;
+	WriteLn('Ingrese los datos del cliente');
+	WriteLn('Apellido: ');
+	ReadLn(Ape);
+	ValidarPalabra(Ape);
+	WriteLn('Nombre: ');
+	readln(Nom);
+	ValidarPalabra(Nom);
+	WriteLn('Codigo: ');
+	ReadLn(Cod);
+	ValidarCodigo(Cod);
+	Cliente.Apellido:= Ape;
+	Cliente.Nombre:= Nom;
+	Cliente.Codigo:= Cod;
+	reset(Client);
+	while (not EOF(Client)) do begin
+	  Read(Client, ClienteAux);
+	  if ((Cliente.Apellido = ClienteAux.Apellido) and (Cliente.Nombre = ClienteAux.Nombre) and (Cliente.Codigo = ClienteAux.Codigo) and (ClienteAux.Activo = True)) then begin
+		ClienteAux.Activo:= False;
+		Seek(Client, (FilePos(Client) - 1));
+		write(Client, ClienteAux);
+		Flag:= True;
+	  end;
+	end;
+	Close(Client);
+	if (Flag = True) then begin
+	  writeln('Cliente dado de baja con exito');
+	end else 
+		WriteLn('El cliente indicado no fue encontrado');
+
+end;
+
+
+//--------------------------------------------------------------------
+//MODIFICACION
+
+Function ValidarOpcionMod(Op: String): Boolean;
+var
+	i, codigoDeError: integer;
+begin
+
+	ValidarOpcionMod:= False;
+	i:= 0;
+	val(Op, i, codigoDeError);
+	if (codigoDeError = 0) then begin
+		if ((i >= 0) and (i <= 3)) then
+	  		ValidarOpcionMod:= True;
+	end;
+
+end;
+
+Procedure Modificacion(var Client: archivoClientes);
+var	
+	Cliente, ClienteAux: regCliente;
+	seguir: Boolean;
+	OpcAux: String;
+	Opc: Integer;
+	Nom, Ape, Cod: String;
+	a: char;
+	Pos: Integer;
+	bandera: Boolean;
+begin
+  
+	seguir:= True;
+	bandera:= False;
+	WriteLn('Ingrese los datos del cliente');
+	WriteLn('Apellido: ');
+	ReadLn(Ape);
+	ValidarPalabra(Ape);
+	WriteLn('Nombre: ');
+	readln(Nom);
+	ValidarPalabra(Nom);
+	WriteLn('Codigo: ');
+	ReadLn(Cod);
+	ValidarCodigo(Cod);
+	Cliente.Apellido:= Ape;
+	Cliente.Nombre:= Nom;
+	Cliente.Codigo:= Cod;
+	if (esta(Client, Cliente) = True) then begin
+		while (seguir = true) do begin
+	 		writeln('Indique lo que desea modificar');
+	  		writeln('1: Apellido');
+	  		writeln('2: Nombre');
+	  		writeln('3: Codigo');
+	  		writeln('0: Salir');
+	  		ReadLn(OpcAux);
+	  	if (ValidarOpcionMod(OpcAux) = True) then begin
+			Opc:= ValorEntero(OpcAux);
+			case (Opc) of
+				1:begin
+			 		writeln('Ingrese el nuevo apellido');
+			  		readln(Ape);
+			  		ValidarPalabra(Ape);
+				end;  
+				2:begin
+			  		writeln('Ingrese el nuevo nombre');
+			  		readln(Nom);
+			  		ValidarPalabra(Nom);
+				end;
+				3:begin
+			  		writeln('Ingrese nuevo codigo');
+			  		readln(Cod);
+			  		ValidarCodigo(Cod);
+				end;
+				0:begin
+			  		clrscr;
+			  		seguir:= False;
+			  		writeln('Saliendo del menu de modificacion');
+				end;
+			end;
+			if (Opc <> 0) then begin
+				writeln('Presiones una tecla para continuar');
+				a:= readkey;
+				clrscr;
+	  		end;
+	  	end else begin
+			clrscr;
+			writeln('Error, ', OpcAux, ' no es un dato permitido, por favor ingrese un numero que corresponda a las opcines en pantalla');
+	  		end;
+		end;
+
+		reset(Client);
+		while (not EOF(Client)) do begin
+		  read(Client, ClienteAux);
+		  if ((Cliente.Apellido = ClienteAux.Apellido) and (Cliente.Nombre = ClienteAux.Nombre) and (Cliente.Codigo = ClienteAux.Codigo) and (ClienteAux.Activo = True)) then begin
+			ClienteAux.Apellido:= Ape;
+			ClienteAux.Nombre:= Nom;
+			ClienteAux.Codigo:= Cod;
+			Seek(Client, (FilePos(Client) - 1));
+			Write(Client, ClienteAux);
+		  end;
+		end;
+		Close(Client);
+	end else 
+		WriteLn('No se encontro al cliente dado');
+
+end;
+
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+var
+	Clientes: archivoClientes;
+	Opcion: Integer;
+	Sigo: Boolean;
+	OpcionAux: String;
+	c: char;
+BEGIN 
+
+	Sigo:= True;
+	assign(Clientes, 'C:\Base de Datos 2022\ProgramasPascal\Ej1-BD-Version2\clientes.dat');
+	if (not fileexists('C:\Base de Datos 2022\ProgramasPascal\Ej1-BD-Version2\clientes.dat')) then begin
+	  writeln('El archivo no existe, se procede a crear uno');
+	  rewrite(Clientes);
+	end; 
+	while (Sigo = True) do begin
+	  writeln('1 Alta');
+	  writeln('2 Baja');
+	  writeln('3 Modificacion');
+	  writeln('4 Mostrar datos');
+	  writeln('0 Salir');
+	  readln(OpcionAux);
+	  if (ValidarOpcion(OpcionAux) = True) then begin
+	  	Opcion:= ValorEntero(OpcionAux);
+	  	case (Opcion) of
+				1:begin
+			  		clrscr;
+			  		Alta(Clientes);
+				end;
+
+				2:begin
+			  		clrscr;
+			  		Baja(Clientes);
+				end;
+
+				3:begin
+			  		clrscr;
+			 	 	Modificacion(Clientes);
+				end;
+
+				4:begin
+			  		clrscr;
+			  		MostrarDatos(Clientes);
+				end;
+
+				0:begin
+			  		clrscr;
+			  		Sigo:= False;
+			  		writeln('Fin del programa');
+				end;
+	  	end;
+	  	if (Opcion <> 0) then begin
+			writeln('Presiones una tecla para continuar');
+			c:= readkey;
+			clrscr;
+	  	end;
+
+	   end else begin
+	   		clrscr;
+		 	writeln('Error, ', OpcionAux, ' no es un dato permitido, por favor ingrese un numero que corresponda a las opcines en pantalla');
+	   end;
+	end;
+
+END.
---
+++ Ejercicio3.pas
@@ -0,0 +1,595 @@
+program Ejercicio3;
+
+{$MODE OBJFPC}
+
+uses crt, dos, sysutils;
+
+type 
+	metadata = record
+		nombre: String[200];
+		caracteres: integer;
+	end;
+
+	estructura = record
+		dato: String[200];
+	end;
+
+	archivoClientes = file of estructura;
+
+	archivoMetadata = file of metadata;
+
+var
+	Clientes: archivoClientes;
+	aMetadata: archivoMetadata;
+	Opcion, cantRegistros, cantClientes, cantCampos: Integer;
+	Sigo: Boolean;
+	OpcionAux, cantCamposAux: String;
+	archivoEncontrado: Boolean;
+	unCampo, noError: Boolean;
+	
+//----------------------------------------------------------------------	
+//VALIDACIONES
+
+function ValorEntero(texto: string): integer;
+var
+  valor, codigoDeError: integer;
+begin
+  valor:= 0;
+  val(texto, valor, codigoDeError);
+  ValorEntero := valor;
+end;
+
+Procedure ValidarEnterosMayoresACero(var Numero: String);
+begin
+
+	while (ValorEntero(Numero) <= 0) do begin
+		writeln('Error ingrese un numero entero mayor a 0');
+		readln(Numero);
+	end;
+
+end;
+
+
+Function ValidarOpcion(Op: String): Boolean;
+var
+	i, codigoDeError: integer;
+begin
+  
+	ValidarOpcion:= False;
+	i:= 0;
+	val(Op, i, codigoDeError);
+	if (codigoDeError = 0) then begin
+		if ((i >= 0) and (i <= 5)) then
+	  		ValidarOpcion:= True;
+	end;		
+
+end;
+
+
+//--------------------------------------------------------------------
+//ValidarQueSeaUnoDos
+Procedure ValidarSoN(var dato: char);
+begin
+  
+	while ((dato <> 'S') and (dato <> 'N')) do begin
+	  writeln('Error, ingrese S o N');
+	  readln(dato);
+	end;
+
+end;	
+
+
+//--------------------------------------------------------------------
+//VALIDAR QUE SOLO SE INGRESEN LETRAS
+
+function revisarContenido(var Palabra:String) : Boolean;
+var
+  i:Integer;
+  Flag: Boolean;
+begin
+  Flag:= True;
+  if ((Palabra <> '') and (Palabra <> ' ')) then begin
+ 	for i:=1 to Length(Palabra) do
+    	if not (Palabra[i] in ['A'..'Z', 'a'..'z']) then 
+  		Flag:= False;
+	end else 
+		Flag:= False;
+  revisarContenido:=Flag;
+end;
+
+Procedure ValidarPalabra(Var Palabra: String);
+begin
+  while (revisarContenido(Palabra) = False) do begin
+    writeln('Error, ingrese solo letra');
+    readln(Palabra);
+  end;
+
+end;
+
+	
+//----------------------------------------------------------------------
+//CREAR ARCHIVOS
+
+procedure crearArchivoMetaData(var aMetadata: archivoMetadata; var archivoEncontrado: Boolean);
+var 
+	registro:metadata;
+begin
+	assign(aMetadata,'C:\Base de Datos 2022\ProgramasPascal\Ej3-VersionFinal\metadata.dat');
+    if not FileExists('C:\Base de Datos 2022\ProgramasPascal\Ej3-VersionFinal\metadata.dat') then
+      Begin
+        Writeln('El archivo "metadata" no existe, se procede a crear uno');
+        rewrite(aMetadata);
+        seek (aMetadata,filesize(aMetadata));
+        registro.nombre:='Estado';
+        registro.caracteres:=1;
+        write(aMetadata,registro);
+        close(aMetadata);
+      End
+    else
+    Begin
+      Writeln('Se encontro el archivo "metadata"');
+      archivoEncontrado:=True;
+    End;
+end;
+
+Procedure crearArchivoClientes(var Clientes: archivoClientes);
+begin
+
+	assign(Clientes, 'C:\Base de Datos 2022\ProgramasPascal\Ej3-VersionFinal\clientes.dat');
+	if (not FileExists('C:\Base de Datos 2022\ProgramasPascal\Ej3-VersionFinal\clientes.dat')) then begin
+		writeln('El archivo "Clientes" no existe, se procede a crear uno');
+		rewrite(Clientes);
+		Close(Clientes);
+	end else
+		writeln('Se encontro el archivo "Clientes"');
+	
+end;
+
+
+//----------------------------------------------------------------------
+//CARGAR CAMPOS
+
+Procedure cargarCampos(var aMetadata: archivoMetadata; cantCampos: Integer);
+var
+	registro: metadata;
+	nroCampo: Integer;
+	cantCaracteres: String;
+begin
+
+	nroCampo:= 1;
+	while (cantCampos > 0) do begin
+		writeln('Campo numero ', nroCampo);
+		writeln('Ingrese el nombre del campo');
+		readln(registro.nombre);
+		ValidarPalabra(registro.nombre);
+		writeln('Ingrese la cantidad de bytes que contiene el campo');
+		readln(cantCaracteres);
+		ValidarEnterosMayoresACero(cantCaracteres);
+		registro.caracteres:= ValorEntero(cantCaracteres);
+		reset(aMetadata);
+		Seek(aMetadata, FileSize(aMetadata));
+		write(aMetadata, registro);
+		close(aMetadata);
+		cantCampos:= cantCampos - 1;
+		nroCampo:= nroCampo + 1;
+		clrscr;
+	end;
+	writeln('Campos creados con exito');
+	unCampo:= True;
+
+end;
+
+
+//----------------------------------------------------------------------
+//CONTAR CAMPOS
+
+Procedure contarCampos(var aMetadata: archivoMetadata);
+begin
+
+	cantRegistros:= 0;
+	reset(aMetadata);
+	while (not EOF(aMetadata)) do begin
+		Seek(aMetadata, FilePos(aMetadata) + 1);
+		cantRegistros:= cantRegistros + 1;
+	end;
+	close(aMetadata);
+
+end;
+
+
+//----------------------------------------------------------------------
+//CONTAR CLIENTES
+
+Procedure contarClientes();
+begin
+
+	reset(Clientes);
+	cantClientes:= 1;
+	while (not EOF(Clientes)) do begin
+		Seek(Clientes, FilePos(Clientes) + 1);
+		cantClientes:= cantClientes + 1;
+	end;
+	cantClientes:= cantClientes div cantRegistros;
+	Close(Clientes);
+
+end;
+
+
+//----------------------------------------------------------------------
+//VALIDAR POSICION DE CLIENTE VALIDA
+
+Procedure ValidarPosicionClienteIngresada(var Num: String);
+begin
+
+	contarClientes();
+	while ((Num <> '0') and (ValorEntero(Num) < 0) and (ValorEntero(Num) > (cantClientes - 1))) do begin
+		writeln('Error ingrese una posicion entre 0 y ', (cantClientes - 1));
+		readln(Num);
+	end;
+
+end;
+
+
+//----------------------------------------------------------------------
+//MOSTRAR
+
+Procedure MostrarEstructuraRegistro(var aMetadata: archivoMetadata);
+var
+	registro: metadata;
+begin
+
+	reset(aMetadata);
+	cantRegistros:= 0;
+	writeln('======== Estructura ========');
+	while (not EOF(aMetadata)) do begin
+		read(aMetadata, registro);
+		cantRegistros:= cantRegistros + 1;
+		if (not(registro.nombre = 'Estado')) then begin
+			writeln('Campo numero: ', cantRegistros - 1);
+			writeln('Nombre: ', registro.nombre);
+			writeln('Cantidad de caracteres: ', registro.caracteres);
+			writeln('============================');
+		end;
+	end;
+	Close(aMetadata);
+	writeln('Pulse una tecla para continuar');
+	readkey;
+
+end;
+
+
+Procedure MostrarDatos(var aMetadata: archivoMetadata; var Clientes: archivoClientes);
+var
+	regMeta: metadata;
+	registro: estructura;
+	contador: Integer;
+begin
+
+	contador:= 0;
+	reset(Clientes);
+	writeln('============================');
+	while (not EOF(Clientes)) do begin
+		writeln('Cliente numero ', contador);
+		reset(aMetadata);
+		while (not EOF(aMetadata)) do begin
+			read(aMetadata, regMeta);
+			if (not(regMeta.nombre = 'Estado')) then begin
+				write(regMeta.nombre,': ');
+				read(Clientes, registro);
+				write(registro.dato);
+				writeln('');
+			end else
+				Seek(Clientes, FilePos(Clientes) + 1);
+		end;
+		Close(aMetadata);
+		writeln('============================');
+		contador:= contador + 1;
+	end;
+	Close(Clientes);
+	writeln('Pulse una tecla para continuar');
+	readkey;
+
+end;
+
+
+Procedure mostrarCliente(var Clientes: archivoClientes; Numero: Integer);
+var
+	regMeta: metadata;
+	registro: estructura;
+begin
+
+	reset(Clientes);
+	Seek(Clientes, Numero);
+	writeln('Cliente ', (Numero div (cantRegistros - 1)));
+	reset(aMetadata);
+	while (not EOF(aMetadata)) do begin
+		read(aMetadata, regMeta);
+		if (not(regMeta.nombre = 'Estado')) then begin
+			write(regMeta.nombre,': ');
+			read(Clientes, registro);
+			write(registro.dato);
+			writeln('');
+			if (EOF(Clientes)) then
+			break;
+		end else
+			Seek(Clientes, FilePos(Clientes) + 1);
+	end;
+	Close(Clientes);
+	Close(aMetadata);
+
+end;
+
+
+//----------------------------------------------------------------------
+//PROCEDIMIENTO REESTRUCTURAR
+
+Procedure Reestructurar();
+var
+	ClientesB: archivoClientes;
+	registro: estructura;
+begin
+
+	reset(Clientes);
+	assign(ClientesB, 'C:\Base de Datos 2022\ProgramasPascal\Ej3-VersionFinal\clientesB.dat');
+	rewrite(ClientesB);
+	Seek(Clientes, 0);
+	while (not EOF(Clientes)) do begin
+		read(Clientes, registro);
+		if (registro.dato = '0') then begin
+			Seek(Clientes, (FilePos(Clientes) + cantRegistros - 1));
+		end else
+			write(ClientesB, registro);
+	end;
+	Close(Clientes);
+	rewrite(Clientes);
+	Seek(ClientesB, 0);
+	while (not EOF(ClientesB)) do begin
+		read(ClientesB, registro);
+		write(Clientes, registro);
+	end;
+	Close(ClientesB);
+
+end;
+
+//----------------------------------------------------------------------
+//PROCEDIMIENTO DE ALTA
+
+Procedure Alta(var aMetadata: archivoMetadata; var Clientes: archivoClientes);
+var
+	regMeta: metadata;
+	registro: estructura;
+begin
+
+	reset(aMetadata);
+	reset(Clientes);
+	while (not EOF(aMetadata)) do begin
+		read(aMetadata, regMeta);
+		if (regMeta.nombre = 'Estado') then begin
+			registro.dato:= '1';
+			Seek(Clientes, FileSize(Clientes));
+			write(Clientes, registro);
+		end else begin
+			writeln('Ingrese ', regMeta.nombre);
+			readln(registro.dato);
+			//Validar
+			if (Length(registro.dato) > regMeta.caracteres) then begin
+				delete(registro.dato, (regMeta.caracteres + 1), Length(registro.dato));
+			end;
+			Seek(Clientes, FileSize(Clientes));
+			write(Clientes, registro);
+		end;
+	end;
+	Close(aMetadata);
+	Close(Clientes);
+	writeln('Registro dado de alta con exito');
+
+end;
+
+
+//----------------------------------------------------------------------
+//PROCEDIMIENTO DE BAJA
+
+Procedure Baja(var Clientes: archivoClientes);
+var
+	clienteAEliminar: Integer;
+	registro: estructura;
+	respuesta: char;
+	Posicion: String;
+begin
+
+	contarClientes();
+	if (cantClientes = 0) then begin
+		writeln('No hay clientes registrados');
+		writeln('Presione una tecla para volver al menu principal');
+		readkey;
+		exit;
+	end;
+	writeln('Ingrese el cliente que desea dar de baja');
+	readln(Posicion);
+	ValidarPosicionClienteIngresada(Posicion);
+	clienteAEliminar:= ValorEntero(Posicion);
+	clienteAEliminar:= clienteAEliminar * cantRegistros;
+	mostrarCliente(Clientes, clienteAEliminar);
+	reset(Clientes);
+	Seek(Clientes, clienteAEliminar);
+	writeln('Desea dar de baja al cliente. (S/N)');
+	readln(respuesta);
+	respuesta:= UpCase(respuesta);
+	ValidarSoN(respuesta);
+	if (Respuesta = 'S') then begin
+		read(Clientes, registro);
+		registro.dato:= '0';
+		Seek(Clientes, clienteAEliminar);
+		write(Clientes, registro);
+		Close(Clientes);
+		Reestructurar();
+		writeln('Cliente dado de baja con exito');
+	end else
+		Close(Clientes);
+
+end;
+
+
+//----------------------------------------------------------------------
+//PROCEDIMIENTO DE MODIFICAR
+
+Procedure Modificacion(var aMetadata: archivoMetadata; var Clientes: archivoClientes);
+var
+	clienteAModificar: Integer;
+	regMeta: metadata;
+	registro: estructura;
+	respuesta: char;
+	Posicion: String;
+begin
+
+	contarClientes();
+	if (cantClientes = 0) then begin
+		writeln('No hay clientes registrados');
+		writeln('Precione una tecla para volver al menu principal');
+		readkey;
+		exit;
+	end;
+	writeln('Ingrese el cliente que desea modificar');
+	readln(Posicion);
+	ValidarPosicionClienteIngresada(Posicion);
+	clienteAModificar:= ValorEntero(Posicion);
+	clienteAModificar:= clienteAModificar * cantRegistros;
+	mostrarCliente(Clientes, clienteAModificar);
+	reset(Clientes);
+	Seek(Clientes, clienteAModificar);
+	reset(aMetadata);
+	Seek(aMetadata, 0);
+	while (not EOF(aMetadata)) do begin
+		read(aMetadata, regMeta);
+		if (not(regMeta.nombre = 'Estado')) then begin
+			writeln('Desea modificar ', regMeta.nombre, ' (S/N)');
+			readln(respuesta);
+			respuesta:= UpCase(respuesta);
+			ValidarSoN(respuesta);
+			read(Clientes, registro);
+			if (respuesta = 'S') then begin
+				writeln('Ingrese ', regMeta.nombre);
+				readln(registro.dato);
+				if (length(registro.dato) > regMeta.caracteres) then begin
+					delete(registro.dato, regMeta.caracteres, length(registro.dato));
+				end;
+				Seek(Clientes, FilePos(Clientes) - 1);
+				write(Clientes, registro);
+			end;
+		end else
+			Seek(Clientes, FilePos(Clientes) + 1);
+	end;
+	Close(aMetadata);
+	Close(Clientes);
+	writeln('Registro modificado con exito');
+
+end;
+
+
+//----------------------------------------------------------------------
+
+BEGIN
+
+	Sigo:= True;
+	noError:= True;
+	unCampo:= False;
+	archivoEncontrado:= False;
+	crearArchivoMetadata(aMetadata, archivoEncontrado);
+	if (archivoEncontrado = False) then begin
+		while (Sigo = True) do begin
+	  		writeln('1 crear campo');
+	  		writeln('0 Salir');
+	  		readln(OpcionAux);
+	  	if (ValidarOpcion(OpcionAux) = True) then begin
+	  		Opcion:= ValorEntero(OpcionAux);
+	  		case (Opcion) of
+				1:begin
+					clrscr;
+					writeln('Ingrese la cantidad de campos que tendra la lista de clientes');
+					readln(cantCamposAux);
+					ValidarEnterosMayoresACero(cantCamposAux);
+					cantCampos:= ValorEntero(cantCamposAux);
+					cargarCampos(aMetadata, cantCampos);
+				end;
+
+				0:begin
+			  		clrscr;
+					if (unCampo = False) then begin
+			  			writeln('El archivo de metadata debe contener al menos un campo');
+					end else begin
+						Sigo:= False;
+						archivoEncontrado:= True;
+			  			writeln('');
+					end;
+				end;
+	  		end;
+	  		if (Opcion <> 0) then begin
+				writeln('Presiones una tecla para continuar');
+				readkey;
+				clrscr;
+	  		end;
+
+	   	end else begin
+	   		clrscr;
+		 	writeln('Error, ', OpcionAux, ' no es un dato permitido, por favor ingrese un numero que corresponda a las opcines en pantalla');
+	   	end;
+	end;
+	end;
+	
+	Sigo:= True;
+	contarCampos(aMetadata);
+	crearArchivoClientes(Clientes);
+	while (Sigo = True) do begin
+	  writeln('1 Alta');
+	  writeln('2 Baja');
+	  writeln('3 Modificacion');
+	  writeln('4 Mostrar datos');
+	  writeln('5 Mostrar estructura del registro');
+	  writeln('0 Salir');
+	  readln(OpcionAux);
+	  if (ValidarOpcion(OpcionAux) = True) then begin
+	  	Opcion:= ValorEntero(OpcionAux);
+	  	case (Opcion) of
+				1:begin
+			  		clrscr;
+			  		Alta(aMetadata, Clientes);
+				end;
+
+				2:begin
+			  		clrscr;
+			  		Baja(Clientes);
+				end;
+
+				3:begin
+			  		clrscr;
+			 	 	Modificacion(aMetadata, Clientes);
+				end;
+
+				4:begin
+			  		clrscr;
+			  		MostrarDatos(aMetadata, Clientes);
+				end;
+
+				5:begin
+			  		clrscr;
+			  		MostrarEstructuraRegistro(aMetadata);
+				end;
+
+				0:begin
+			  		clrscr;
+			  		Sigo:= False;
+			  		writeln('Fin del programa');
+				end;
+	  	end;
+	  	if (Opcion <> 0) then begin
+			writeln('Presiones una tecla para continuar');
+			readkey;
+			clrscr;
+	  	end;
+	   end else begin
+	   		clrscr;
+		 	writeln('Error, ', OpcionAux, ' no es un dato permitido, por favor ingrese un numero que corresponda a las opcines en pantalla');
+	   end;
+	end;
+
+END.
