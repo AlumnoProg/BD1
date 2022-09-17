bcdf05f4428cb2fe8f612d0fbe51a8c7916fcd01
lucasrueda01 <71470879+lucasrueda01@users.noreply.github.com>
2022-09-17 19:03:11 -0300

    mejoras

--- Ejercicio2.pas
+++ Ejercicio2.pas
@@ -56,7 +56,7 @@ begin
     proximaPosicionLibreZO := PosZO;
 end;
 
-function ingresarDatos(): Cliente;
+function ingresarDatos(Codigo:Integer): Cliente;
 var 
     RC: Cliente;
 begin
@@ -64,8 +64,7 @@ begin
     Readln(RC.Nombre);
     Write('Apellido: ');
     ReadLn(RC.Apellido);
-    Write('Codigo de cliente: ');
-    ReadLn(RC.Codigo);
+    RC.Codigo := Codigo;
     while (RC.Codigo <= 0) or (RC.Codigo > 9999) do begin //validacion maximo 4 digitos
         WriteLn('-Ingrese un codigo valido-');
         Write('Codigo de cliente: ');
@@ -155,50 +154,73 @@ procedure alta(C:Cliente);
 var 
     PosReg, PosZO: integer;
     RegDeArchivo: Cliente;
+    Existe:Boolean;
 begin
+    Existe := existeCodigo(C.Codigo);
     Reset(A);
     PosReg := funcionHashing(C.Codigo);
     seek(A,PosReg);
     Read(A,RegDeArchivo);
-    if (not RegDeArchivo.Ocupado) then begin //si no esta ocupada en la tabla Hash, agregarla
-        seek(A,PosReg);
-        C.ACTIVO := True;
-        C.Ocupado := True;
-        Write(A,C);      
-        WriteLn('Registro guardado en la posicion: ' + PosReg.ToString);
-    end 
-    else if (RegDeArchivo.Siguiente = -1) then begin // ocupada sin colisiones, agregar en ZO
-        PosZO := proximaPosicionLibreZO();
-        seek(A,PosZO);
-        C.ACTIVO := True;
-        C.Ocupado := True;
-        Write(A,C);
-        //Colocar la direccion de la primera colision
-        seek(A,PosReg);
-        Read(A,C);
-        C.Siguiente := PosZO;
-        seek(A,PosReg);
-        Write(A,C);
-        WriteLn('Colision detectada. Se guardara en la Zona de OverFlow');
-    end
-    else begin //si ya tiene colisiones, agregarla al final de estas
-        //meto el registro
-        PosZO := proximaPosicionLibreZO();
-        seek(A,PosZO);
-        C.ACTIVO := True;
-        C.Ocupado := True;
-        Write(A,C);
-        //busco la colision anterior para guardarle la posicion de la actual colision
-        seek(A,PosReg);
-        Read(A,RegDeArchivo);
-        while (RegDeArchivo.Siguiente <> -1) do begin
-            seek(A,RegDeArchivo.Siguiente);
-            Read(A,RegDeArchivo); 
+    if not Existe then begin  // Agregar un nuevo registro 
+        if (not RegDeArchivo.Ocupado) then begin //si no esta ocupada en la tabla Hash, agregarla
+            seek(A,PosReg);
+            C.ACTIVO := True;
+            C.Ocupado := True;
+            Write(A,C);      
+            WriteLn('Registro guardado en la posicion: ' + PosReg.ToString);
+        end 
+        else if (RegDeArchivo.Siguiente = -1) then begin // ocupada sin colisiones, agregar en ZO
+            PosZO := proximaPosicionLibreZO();
+            seek(A,PosZO);
+            C.ACTIVO := True;
+            C.Ocupado := True;
+            Write(A,C);
+            //Colocar la direccion de la primera colision
+            seek(A,PosReg);
+            Read(A,C);
+            C.Siguiente := PosZO;
+            seek(A,PosReg);
+            Write(A,C);
+            WriteLn('Colision detectada. Se guardara en la Zona de OverFlow');
+        end
+        else begin //si ya tiene colisiones, agregarla al final de estas
+            //meto el registro
+            PosZO := proximaPosicionLibreZO();
+            seek(A,PosZO);
+            C.ACTIVO := True;
+            C.Ocupado := True;
+            Write(A,C);
+            //busco la colision anterior para guardarle la posicion de la actual colision
+            seek(A,PosReg);
+            Read(A,RegDeArchivo);
+            while (RegDeArchivo.Siguiente <> -1) do begin
+                seek(A,RegDeArchivo.Siguiente);
+                Read(A,RegDeArchivo); 
+            end;
+            seek(A,FilePos(A) - 1);
+            RegDeArchivo.Siguiente := PosZO;
+            Write(A,RegDeArchivo);
+            WriteLn('Multiples colisiones detectadas. Se guardara en la Zona de OverFlow');
         end;
-        seek(A,FilePos(A) - 1);
-        RegDeArchivo.Siguiente := PosZO;
-        Write(A,RegDeArchivo);
-        WriteLn('Multiples colisiones detectadas. Se guardara en la Zona de OverFlow');
+    end
+
+    else begin // si ya existe buscar el registro y darlo de alta
+        if RegDeArchivo.Codigo = C.Codigo then begin //posicion original
+            RegDeArchivo.ACTIVO := True;
+            Seek(A,PosReg);
+            Write(A,RegDeArchivo);
+        end else begin
+            while RegDeArchivo.Siguiente <> -1 do begin //busca en las colisiones
+                Seek(A,RegDeArchivo.Siguiente);
+                Read(A,RegDeArchivo);
+                if RegDeArchivo.Codigo = C.Codigo then begin
+                    RegDeArchivo.ACTIVO := True;
+                    Seek(A,RegDeArchivo.Siguiente);
+                    Write(A,RegDeArchivo);
+                    Break;
+                end;
+            end; 
+        end;
     end;
     Close(A);   
 end;
@@ -212,12 +234,12 @@ begin
     PosReg := funcionHashing(Codigo);
     Seek(A,PosReg);
     Read(A,RegDeArchivo);
-    if RegDeArchivo.Codigo = Codigo then begin
+    if RegDeArchivo.Codigo = Codigo then begin //posicion original
         RegDeArchivo.ACTIVO := False;
         Seek(A,PosReg);
         Write(A,RegDeArchivo);
     end else begin
-        while RegDeArchivo.Siguiente <> -1 do begin
+        while RegDeArchivo.Siguiente <> -1 do begin //busca en las colisiones
             Seek(A,RegDeArchivo.Siguiente);
             Read(A,RegDeArchivo);
             if RegDeArchivo.Codigo = Codigo then begin
@@ -267,6 +289,7 @@ begin
 end;
 
 
+
 //-----------------------PROGRAMA PRINCIPAL--------------------------
 
 BEGIN
@@ -290,12 +313,18 @@ BEGIN
 
         case Opcion of
             1: begin
-                RegistroCliente := ingresarDatos();
-                if not existeCodigo(RegistroCliente.Codigo) then
-                    alta(RegistroCliente)
-                else
-                    WriteLn('-El codigo ingresado ya existe-');
-                continuar();
+                Write('Ingrese el codigo a dar de alta: ');
+                ReadLn(RegistroCliente.Codigo);
+                if not existeCodigo(RegistroCliente.Codigo) then begin
+                    RegistroCliente := ingresarDatos(RegistroCliente.Codigo);
+                    alta(RegistroCliente);
+                    continuar()
+                end
+                else begin
+                    WriteLn('El codigo ingresado existe. Dando de alta...');
+                    alta(RegistroCliente); 
+                    continuar();    
+                end;
             end;
             2: begin
                 write('Ingrese el codigo a dar de baja: ');   
