c260aa6cfd4571c6e00ca3520f93050f4ecb6c85
AlumnoProg <113617007+AlumnoProg@users.noreply.github.com>
2022-11-12 16:44:51 -0300

    Update Ej5.sql

--- TP-8/Ej5/Ej5.sql
+++ TP-8/Ej5/Ej5.sql
@@ -2,7 +2,7 @@ CREATE VIEW EJ5 (ID_CLIENTE, APELLIDO_Y_NOMBRE)
 
 /* hacemos un inner join entre cliente y poliza, con eso hacemos un left join a accidente y nos quedamos con los que no han tenido accidentes (aquellos con campos de accidente nulos) */
 
-AS select c.ID_CLIENTE, c.APE_NOM 
+AS select distinct c.ID_CLIENTE, c.APE_NOM 
     from CLIENTE c INNER JOIN POLIZA p ON c.ID_CLIENTE = p.ID_CLIENTE
     LEFT JOIN ACCIDENTE a on p.NUMERO_POLIZA = a.NUMERO_POLIZA 
     where a.DESCRIPCION is null;
