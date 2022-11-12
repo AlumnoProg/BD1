0c81ad3360d86b8a7fe788527250a672e9a2d2f9
AlumnoProg <113617007+AlumnoProg@users.noreply.github.com>
2022-11-12 16:48:36 -0300

    Update Ej7.sql

--- TP-8/Ej7/Ej7.sql
+++ TP-8/Ej7/Ej7.sql
@@ -3,7 +3,7 @@ CREATE VIEW EJ7 (CUIT, RAZON_SOCIAL, CANTIDAD_TOTAL)
 /* Seleccionamos el maximo valor de cant de producto de la tabla con los proveedores y el total de los productos vendidos */
 
 AS select v.cuit, v.razon_social, v.cantidad from CANTIDAD_PRODUCTO_PROVEEDOR v
-    where v.CANTIDAD = (select max(v.cantidad) from CANTIDAD_PRODUCTO_PROVEEDOR v);
+    where v.CANTIDAD = (select max(vv.cantidad) from CANTIDAD_PRODUCTO_PROVEEDOR vv);
 
 GRANT DELETE, INSERT, REFERENCES, SELECT, UPDATE
  ON EJ7 TO  SYSDBA WITH GRANT OPTION;
